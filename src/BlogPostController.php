ede869cd52026ff8b32fda190f3cad3051b88288
Jack Marchant <jack@jackmarchant.com>
2020-06-03 17:38:08 +1000

    fix 404 page and rename blog title

--- src/BlogPostController.php
+++ src/BlogPostController.php
@@ -27,19 +27,20 @@ class BlogPostController
     {
         $this->logger->info('blog post handler dispatched');
 
-        $post = [];
+        $post = ['title' => 'Page not found'];
     
         if (isset($args['post'])) {
-            $filepath = sprintf('/../content/%s/index.md', $args['post']);
-            $content = file_get_contents(__DIR__ . $filepath);
-            $exploded = explode('---', $content);
-            $metadata = $this->parseMetadata($exploded[1]);
-            $post = [
-                'title' => $metadata['title'],
-                'date' => (new DateTime($metadata['date']))->format('F d, Y'),
-                'content' => $this->markdown->text($exploded[2]),
-            ];
-            $this->logger->info($post['content']);
+            $filepath = __DIR__ . sprintf('/../content/%s/index.md', $args['post']);
+            if (file_exists($filepath)) {
+                $content = file_get_contents($filepath);            
+                $exploded = explode('---', $content);
+                $metadata = $this->parseMetadata($exploded[1]);
+                $post = [
+                    'title' => $metadata['title'],
+                    'date' => (new DateTime($metadata['date']))->format('F d, Y'),
+                    'content' => $this->markdown->text($exploded[2]),
+                ];
+            }
         }
     
         $response->getBody()->write(
