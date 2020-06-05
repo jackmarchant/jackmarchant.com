d290e603ef54bacaf710f0d46225da035592f086
Jack Marchant <jack@jackmarchant.com>
2020-06-05 16:30:49 +1000

    Properly handle trailing slashes

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,32 @@
 
 use App\BlogListController;
 use App\BlogPostController;
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->add(function (Request $request, Response $response, callable $next) {
+    $uri = $request->getUri();
+    $path = $uri->getPath();
+    if ($path != '/' && substr($path, -1) == '/') {
+        // recursively remove slashes when its more than 1 slash
+        while(substr($path, -1) == '/') {
+            $path = substr($path, 0, -1);
+        }
+
+        // permanently redirect paths with a trailing slash
+        // to their non-trailing counterpart
+        $uri = $uri->withPath($path);
+        
+        if ($request->getMethod() == 'GET') {
+            return $response->withRedirect((string) $uri, 301);
+        }
+        else {
+            return $next($request->withUri($uri), $response);
+        }
+    }
+
+    return $next($request, $response);
+});
 
 $app->get('/{post}', BlogPostController::class);
 $app->get('/', BlogListController::class);
